375f272106dbe64671a2d87389e6bd6d9ee06fef
= <camposdlucasoli@gmail.com>
2025-04-01 17:20:44 -0300

    Versão mobile feita

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 export const HomeComponent = () => {
   return (
     <div className="flex w-full h-screen bg-gray-50 font-inter">
-      <div className="flex flex-col justify-evenly w-full px-10 items-center gap-y-10">
-        <div className="relative flex flex-col h-[25%] w-full justify-end pb-6 pl-6 items-start bg-gradient-to-r from-green-500 to-green-700 rounded-b-3xl border border-green-600 shadow-lg">
-          <h1 className="text-7xl font-bold text-white tracking-wide">
-            Dack
-            <span className="text-green-200">Ify</span>
+      <div className="flex flex-col justify-evenly w-full px-6 items-center gap-y-8">
+        {/* Cabeçalho */}
+        <div className="relative flex flex-col h-[20%] w-full justify-end pb-4 pl-4 items-start bg-gradient-to-r from-green-500 to-green-700 rounded-b-3xl border border-green-600 shadow-lg">
+          <h1 className="text-5xl md:text-7xl font-bold text-white tracking-wide">
+            Dack<span className="text-green-200">Ify</span>
           </h1>
         </div>
 
         {/* Texto principal */}
-        <div className="flex flex-col justify-center h-[80%]">
-          <h2 className="text-[60px] text-gray-900 font-semibold text-center leading-tight">
+        <div className="flex flex-col justify-center h-[60%] px-2">
+          <h2 className="text-3xl md:text-5xl text-gray-900 font-semibold text-center leading-tight">
             Crie FlashCards com{" "}
             <span className="text-green-600 font-bold">IA</span> para
             <br />
@@ -25,12 +25,12 @@ export const HomeComponent = () => {
         </div>
 
         {/* Botão de ação */}
-        <div className="flex h-[70%] w-full justify-center items-center">
+        <div className="flex h-[50%] w-full justify-center items-center">
           <Link
             to={"/createdDeck"}
-            className="bg-green-600 hover:bg-green-700 rounded-lg w-[280px] h-[70px] text-[28px] font-semibold uppercase text-white flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg"
+            className="bg-green-600 hover:bg-green-700 rounded-lg w-[220px] md:w-[280px] h-[60px] md:h-[70px] text-[22px] md:text-[28px] font-semibold uppercase text-white flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg"
           >
-            Comecar
+            Começar
           </Link>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-export default {
+export default {  
+  content: ['./index.html', './src/**/*,{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       boxShadow: {
