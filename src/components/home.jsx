d3ba767963a1cbe496f91be459a07fafc25dbc80
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-04-12 01:29:54 -0300

    formulario de teste na tela home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,37 +1,108 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FormForTestingNotIA } from "./formForTesting";
+import { Link } from "react-router";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const HomeComponent = () => {
+  const [animatedText, setAnimatedText] = useState("");
+  const [activeForm, setActiveForm] = useState(null);
+  const fullText = "C rie seus Flashcards com auxílio da IA";
+
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setAnimatedText((prev) => prev + fullText.charAt(index));
+      index++;
+      if (index === fullText.length) {
+        clearInterval(intervalId);
+      }
+    }, 140);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      localStorage.clear();
+    };
+  }, []);
+
   return (
-    <div className="flex w-full h-screen bg-gray-50 font-inter">
-      <div className="flex flex-col justify-evenly w-full px-6 items-center gap-y-8">
-        {/* Cabeçalho */}
-        <div className="relative flex flex-col h-[20%] w-full justify-end pb-4 pl-4 items-start bg-gradient-to-r from-green-500 to-green-700 rounded-b-3xl border border-green-600 shadow-lg">
-          <h1 className="text-5xl md:text-7xl font-bold text-white tracking-wide">
-            Deck<span className="text-green-200">Ify</span>
-          </h1>
-        </div>
+    <div className="flex flex-col w-full h-screen bg-gray-50 font-inter">
+      <header className="flex items-center justify-between bg-gray-50 text-green-600 h-20 px-10 shadow-md transition-all ease-in-out duration-300 hover:bg-green-100">
+        <h1 className="font-bold text-3xl sm:text-4xl font-winky cursor-pointer transform transition-all duration-500 ease-out hover:text-green-700">
+          DeckIfy - Crie Flashcards com IA
+        </h1>
 
-        {/* Texto principal */}
-        <div className="flex flex-col justify-center h-[60%] px-2">
-          <h2 className="text-3xl md:text-5xl text-gray-900 font-semibold text-center leading-tight">
-            Crie FlashCards com{" "}
-            <span className="text-green-600 font-bold">IA</span> para
-            <br />
-            Estudar de forma mais eficiente <br />
-            <span className="text-green-700 border-b-4 border-green-500">
-              100% Gratuito
-            </span>
-          </h2>
-        </div>
-
-        {/* Botão de ação */}
-        <div className="flex h-[50%] w-full justify-center items-center">
+        <div className="hidden sm:flex items-center space-x-5">
           <Link
-            to={"/createdDeck"}
-            className="bg-green-600 hover:bg-green-700 rounded-lg w-[220px] md:w-[280px] h-[60px] md:h-[70px] text-[22px] md:text-[28px] font-semibold uppercase text-white flex items-center justify-center transition-all duration-300 shadow-md hover:shadow-lg"
+            to={"https://github.com/lucasoliveira04"}
+            className="text-lg font-medium text-green-600 hover:text-green-800"
           >
-            Começar
+            <FaGithub />
           </Link>
+          <Link
+            to={"https://www.linkedin.com/in/lucas-oliveira-campos"}
+            className="text-lg font-medium text-green-600 hover:text-green-800"
+          >
+            <FaLinkedin />
+          </Link>
+        </div>
+      </header>
+
+      <div className="flex w-full h-full">
+        <div className="flex flex-col justify-center items-start w-[60%] bg-gray-50 p-10 md:p-16 shadow-2xl border border-gray-200">
+          <h2 className="text-6xl md:text-[80px] text-gray-900 font-semibold mb-6 font-anton">
+            Agora a IA pode automatizar seus estudos
+          </h2>
+          <h3 className="text-2xl md:text-3xl text-gray-700 font-medium mb-10">
+            {animatedText}
+          </h3>
+
+          <Link to={"/createdDeck"}>
+            <button className="bg-green-500 p-5 md:p-7 rounded-xl text-xl md:text-2xl font-bold text-white hover:bg-green-600 transition-all">
+              CRIAR FLASHCARDS
+            </button>
+          </Link>
+        </div>
+        <div
+          className={`flex flex-col border ${
+            activeForm === null ? "place-items-center pt-4" : ""
+          } w-[40%] bg-gray-50`}
+        >
+          {activeForm === null && (
+            <div className="flex flex-col items-center justify-center pb-[400px] w-full h-full border-black">
+              <h2 className="text-[40px] font-poppins">Faça um teste</h2>
+              <div className="flex space-x-5 mt-10">
+                <button
+                  className="w-52 h-20 bg-gradient-to-r from-blue-400 to-blue-600 text-white rounded-xl shadow-md text-2xl font-bold transform transition duration-500 ease-smooth hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  onClick={() => setActiveForm("ia")}
+                >
+                  💡 Usar IA
+                </button>
+                <button
+                  className="w-52 h-20 bg-gradient-to-r from-gray-300 to-gray-400 text-gray-800 rounded-xl shadow-md text-2xl font-bold transform transition duration-500 ease-smooth hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-gray-400"
+                  onClick={() => setActiveForm("noIA")}
+                >
+                  🚫 Sem IA
+                </button>
+              </div>
+            </div>
+          )}
+
+          <div
+            className={`transition-opacity duration-500 ease-in-out transform ${
+              activeForm === null
+                ? "opacity-0 pointer-events-none"
+                : "opacity-100"
+            }`}
+          >
+            {activeForm === "noIA" && (
+              <FormForTestingNotIA setActiveForm={setActiveForm} />
+            )}
+
+            {activeForm === "ia" && <FormForTestingNotIA />}
+          </div>
         </div>
       </div>
     </div>
