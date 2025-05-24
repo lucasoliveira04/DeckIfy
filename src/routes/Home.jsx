76c287312c0739baffedd7b42e7ec8a449863cef
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-23 22:33:31 -0300

    fix: aumentando velocidade de conexão com o servidor em 90%

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
 import { FooterComponent } from "../components/footer";
 import { HomeComponent } from "../components/home";
 
 export const HomePage = () => {
+  useEffect(() => {
+    const startApis = async () => {
+      try {
+        const generateAsk = await fetch(
+          "https://anki-convert-file-api.onrender.com/start",
+          {
+            method: "GET",
+          }
+        );
+        const convertToApkg = await fetch(
+          "https://api-dackify-ia-1.onrender.com/",
+          {
+            method: "GET",
+          }
+        );
+
+        const generateAskData = await generateAsk.json();
+        const convertToApkgData = await convertToApkg.json();
+
+        console.log("Generate Ask API started:", generateAskData);
+        console.log("Convert to Apkg API started:", convertToApkgData);
+      } catch (error) {
+        console.error("Error starting APIs:", error);
+      }
+    };
+
+    startApis();
+
+    const intervalId = setInterval(startApis, 30000);
+
+    return () => clearInterval(intervalId);
+  });
+
   return (
     <div className="flex flex-col min-h-screen">
       <HomeComponent />
